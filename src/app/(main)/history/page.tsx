4959c002f70ba14e5323a8c3b531025b5cc6c8fe
Free Software <freesoftware720@gmail.com>
2025-06-07 23:25:21 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <RecipeCard recipe

--- src/app/(main)/history/page.tsx
+++ src/app/(main)/history/page.tsx
@@ -28,7 +28,7 @@ export default function HistoryPage() {
           </div>
         </GlassCard>
       ) : (
-        <div className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {history.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} isDetailedView={true} />
           ))}
